6be5ad59c06da68acdc39672f45644ffae106943
marmittfull <tiagomarmittt@gmail.com>
2020-11-10 19:19:33 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
       })
   }
   const deleteFood = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`).then(()=> {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
       listFoods()
     })
   }
@@ -56,83 +56,98 @@ const App = () => {
     setNewImg('')
   }
   return (
-    <div className="container my-3">
-      <h4 className="text-primary">CRUD using MongoDB, Express, React and Node</h4>
-      <div className="border-top">
-        <div className="input-group mt-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text bg-primary" id="basic-addon1">
-              <i className="far fa-burger-soda text-white"></i>
-            </span>
+    <div>
+      <div className="container my-3">
+        <h4 className="text-primary">CRUD using MongoDB, Express, React and Node</h4>
+        <div className="border-top">
+          <div className="input-group mt-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text bg-primary" id="basic-addon1">
+                <i className="far fa-burger-soda text-white"></i>
+              </span>
+            </div>
+            <input onChange={(event) => { setFoodName(event.target.value) }} value={foodName} type="text" className="form-control" placeholder="Food name" />
           </div>
-          <input onChange={(event) => { setFoodName(event.target.value) }} value={foodName} type="text" className="form-control" placeholder="Food name" />
-        </div>
-        <div className="input-group mt-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text bg-primary text-white">
-              R$
+          <div className="input-group mt-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text bg-primary text-white">
+                R$
             </span>
+            </div>
+            <input onChange={(event) => { setPrice(event.target.value) }} value={price} type="number" className="form-control" placeholder="Price" />
           </div>
-          <input onChange={(event) => { setPrice(event.target.value) }} value={price} type="number" className="form-control" placeholder="Price" />
-        </div>
-        <div className="input-group mt-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text bg-primary">
-              <i className="far fa-images text-white"></i>
-            </span>
+          <div className="input-group mt-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text bg-primary">
+                <i className="far fa-images text-white"></i>
+              </span>
+            </div>
+            <input onChange={(event) => { setImg(event.target.value) }} value={img} type="text" className="form-control" placeholder="Food image link (optional)" />
           </div>
-          <input onChange={(event) => { setImg(event.target.value) }} value={img} type="text" className="form-control" placeholder="Food image link (optional)" />
-        </div>
-        <small className="text-muted">Insert a link to put as your food photo. Right size: 300x300</small>
-        <button onClick={addToList} className="btn btn-primary mt-4 col-12">Add to list</button>
-        <div className="row mt-4">
-          {
-            foodList && foodList.map((item, key) => {
-              return editingItem === item._id ?
-                <div className="col-md-3 mb-2" key={key}>
-                  <div className="card h-100">
-                    <div style={{ height: 200 }} className="align-items-center justify-content-center d-flex">
-                      <input onChange={(event) => { setNewImg(event.target.value) }} defaultValue={item.img} style={{ width: '90%' }} type="text" className="form-control" />
-                    </div>
-                    <div className="card-body">
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text bg-primary" id="basic-addon1">
-                            <i className="far fa-burger-soda text-white"></i>
-                          </span>
-                        </div>
-                        <input onChange={(event) => { setNewFoodName(event.target.value) }} type="text" className="form-control" placeholder="Food name" defaultValue={item.foodName} />
+          <small className="text-muted">Insert a link to put as your food photo. Right size: 300x300</small>
+          <button onClick={addToList} className="btn btn-primary mt-4 col-12">Add to list</button>
+          <div className="row mt-4">
+            {
+              foodList && foodList.map((item, key) => {
+                return editingItem === item._id ?
+                  <div className="col-md-3 mb-2" key={key}>
+                    <div className="card h-100">
+                      <div style={{ height: 200 }} className="align-items-center justify-content-center d-flex">
+                        <input onChange={(event) => { setNewImg(event.target.value) }} defaultValue={item.img} style={{ width: '90%' }} type="text" className="form-control" />
                       </div>
-                      <div className="input-group mt-1">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text bg-primary text-white">
-                            R$
-                          </span>
+                      <div className="card-body">
+                        <div className="input-group">
+                          <div className="input-group-prepend">
+                            <span className="input-group-text bg-primary" id="basic-addon1">
+                              <i className="far fa-burger-soda text-white"></i>
+                            </span>
+                          </div>
+                          <input onChange={(event) => { setNewFoodName(event.target.value) }} type="text" className="form-control" placeholder="Food name" defaultValue={item.foodName} />
                         </div>
-                        <input onChange={(event) => { setNewPrice(event.target.value) }} type="number" className="form-control" placeholder="Price" defaultValue={item.price} />
+                        <div className="input-group mt-1">
+                          <div className="input-group-prepend">
+                            <span className="input-group-text bg-primary text-white">
+                              R$
+                          </span>
+                          </div>
+                          <input onChange={(event) => { setNewPrice(event.target.value) }} type="number" className="form-control" placeholder="Price" defaultValue={item.price} />
+                        </div>
+                        <button type="submit" onClick={() => updateFood(item)} className="btn btn-primary mt-2">Salvar</button>
+                        <button onClick={cancelEditing} className="btn btn-danger ml-2 mt-2">Cancelar</button>
                       </div>
-                      <button type="submit" onClick={() => updateFood(item)} className="btn btn-primary mt-2">Salvar</button>
-                      <button onClick={cancelEditing} className="btn btn-danger ml-2 mt-2">Cancelar</button>
                     </div>
                   </div>
-                </div>
-                :
-                <div className="col-md-3 mb-2" key={key}>
-                  <div className="card h-100">
-                    <img height="250px" width="300px" src={item.img} className="card-img-top" alt={'imagem'} />
-                    <div className="card-body">
-                      <h5 className="card-title">{item.foodName}</h5>
-                      <p className="card-text">R${item.price}</p>
-                      <button onClick={() => setEditingItem(item._id)} className="btn btn-primary">Editar</button>
-                      <button onClick={() => deleteFood(item._id)} className="btn btn-danger ml-2">Excluir</button>
+                  :
+                  <div className="col-md-3 mb-2" key={key}>
+                    <div className="card h-100">
+                      <img height="250px" width="300px" src={item.img} className="card-img-top" alt={'imagem'} />
+                      <div className="card-body">
+                        <h5 className="card-title">{item.foodName}</h5>
+                        <p className="card-text">R${item.price}</p>
+                        <button onClick={() => setEditingItem(item._id)} className="btn btn-primary">Editar</button>
+                        <button onClick={() => deleteFood(item._id)} className="btn btn-danger ml-2">Excluir</button>
+                      </div>
                     </div>
                   </div>
-                </div>
+              }
+              )
             }
-            )
-          }
+          </div>
         </div>
       </div>
+      <footer class="bg-primary p-3 text-light mt-auto">
+        <div class="container d-flex justify-content-start align-items-center">
+          <small>
+            Escola Técnica Estadual Monteiro Lobato
+          <br />
+              Curso Técnico em Informática
+          <br />
+                Disciplina de Criação de Sites
+          <br />
+                  Professor Cândido Farias
+        </small>
+        </div>
+      </footer>
     </div>
   )
 }
